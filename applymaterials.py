ff8550fdf22e4470b942c4574341445d683fd721
Rodrigo Nascimento Hernandez <rodrigo.freebsd@minasambiente.com.br>
2021-02-23 17:07:38 -0300

    add try|except

--- applymaterials.py
+++ applymaterials.py
@@ -10,6 +10,9 @@ actors = unreal.EditorLevelLibrary.get_all_level_actors()
 for each in actors:
     label = each.get_actor_label()
     if 'Landscape' in label:
-        matname = asset_path + material_dir + '/M_' + each.get_path_name().split(':')[0].split('.')[1]
-        mat = unreal.EditorAssetLibrary.load_asset(matname)
-        each.set_editor_property('landscapematerial', mat)
+        try:
+            matname = asset_path + material_dir + '/M_' + each.get_path_name().split(':')[0].split('.')[1]
+            mat = unreal.EditorAssetLibrary.load_asset(matname)
+            each.set_editor_property('landscapematerial', mat)
+        except:
+            pass
